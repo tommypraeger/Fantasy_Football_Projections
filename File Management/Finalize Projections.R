12e0d2fedabdb8a4ffedc144b7be4379a573456a
Jonathan Goldberg <gberg1303@aol.com>
2019-08-14 15:37:51 -0400

    Update Finalize Projections.R

--- File Management/Finalize Projections.R
+++ File Management/Finalize Projections.R
@@ -1,3 +1,7 @@
+### Change 0 ADP
+Average_Fantasy_Projections <- as.data.table(Average_Fantasy_Projections)
+Average_Fantasy_Projections[which(Average_Fantasy_Projections$Average.Draft.Position == 0), Average.Draft.Position := 170]
+
 ### Drop Unimportant Columns and Add Some Other Better Metrics
 Fantasy_Projections <- Average_Fantasy_Projections
 Fantasy_Projections[is.na(Fantasy_Projections)] <- 0
@@ -8,3 +12,7 @@ Fantasy_Projections <- Fantasy_Projections %>%
          Fantasy.Points = Average.Fantasy.Points) %>%
   select(id, Player, Team, Position, Bye, Average.Auction.Value, Cost, Average.Draft.Position, Games.Played, Opportunities, Tier, Value.Rank, VoR, Ceiling.VoR, Floor.VoR,
          Risk, Points.Rank, Fantasy.Points, Ceiling.Fantasy.Points, Floor.Fantasy.Points)
+
+### Round Everything
+Average_Fantasy_Projections[,5:56] <- round(Average_Fantasy_Projections[,5:56], 2)
+Fantasy_Projections[,5:20] <- round(Fantasy_Projections[,5:20], 2)
