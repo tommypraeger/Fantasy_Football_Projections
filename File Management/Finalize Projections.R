fbd42875674dac21b132f77f934dd5f41d4d9dcf
Jonathan Goldberg <gberg1303@aol.com>
2019-08-14 09:09:00 -0400

    Update Finalize Projections.R

--- File Management/Finalize Projections.R
+++ File Management/Finalize Projections.R
@@ -6,5 +6,5 @@ Fantasy_Projections <- Fantasy_Projections %>%
   mutate(Opportunities = (Pass.Attempts + Carries + Receptions), 
          Value.Per.Opportunity = (VoR/Opportunities), 
          Fantasy.Points = Average.Fantasy.Points) %>%
-  select(Player, Team, Position, Bye, Average.Auction.Value, Cost, Average.Draft.Position, Games.Played, Opportunities, Tier, Value.Rank, VoR, Ceiling.VoR, Floor.VoR,
+  select(id, Player, Team, Position, Bye, Average.Auction.Value, Cost, Average.Draft.Position, Games.Played, Opportunities, Tier, Value.Rank, VoR, Ceiling.VoR, Floor.VoR,
          Risk, Points.Rank, Fantasy.Points, Ceiling.Fantasy.Points, Floor.Fantasy.Points)
